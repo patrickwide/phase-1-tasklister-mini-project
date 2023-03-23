1194c3b4747e6522d7589b591486197422504fee
patrickwide <patrickwide254@gmail.com>
2023-03-23 08:38:28 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = taskInput.value;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.setAttribute("data-description", taskInput.value);
+    deleteBtn.addEventListener("click", function (event) {
+      newTask.remove();
+    });
+
+    newTask.appendChild(deleteBtn);
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  });
 });
